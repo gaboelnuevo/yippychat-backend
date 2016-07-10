655cf54a201c1755b4309f0479c23852f1e6ffa5
Gabriel Sabillon <gaboelnuevo@gmail.com>
2016-07-09 20:43:43 -0600

    sendmessage method

--- common/models/message.js
+++ common/models/message.js
@@ -1,3 +1,36 @@
-module.exports = function(Message) {
+var loopback = require('loopback');
 
+module.exports = function(Message) {
+   Message.sendmessage = function(message,cb){
+     var ctx = loopback.getCurrentContext();
+     var currentUser = ctx && ctx.get('currentUser');
+     if(currentUser){
+        Message.create({'content':message.content,
+		      'userId':currentUser.id,
+		      'chanelId': message.chanelId || message.chanelId,
+		      'posted_at':new Date()},
+	      function(err,mess){
+	      		Message.findById(mess.id,{
+  			      include: {relation: 'user'}
+            },function(err,data){
+              // emit socket here
+              if(err){
+                cb();
+              }else{
+                cb(null, data);
+              }
+    			 });
+	      });
+      }else{
+        cb();
+      }
+   };
+
+	Message.remoteMethod('sendmessage', {
+		accepts: [
+	      {arg: 'message', type: 'object', http: { source: 'body' }}
+	     ],
+	    returns: {arg: 'success', type: 'object', root: true},
+	    http: {path:'/sendmessage', verb: 'post'}
+	  });
 };
